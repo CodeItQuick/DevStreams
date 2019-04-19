f8a6c44572fe2d2aff983e1a45394951d57a7e5c
Evan Theriault <evanontario@hotmail.com>
2019-04-19 12:43:28 -0300

    these show be discarded
    discard

---
+++ src/DevChatter.DevStreams.Core/Model/TwitchSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevChatter.DevStreams.Core.Model.Configuration
+{
+    class TwitchSettings
+    {
+        public string ClientId{get; set;}
+    }
+}
